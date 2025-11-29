ad067a1c2ce85ed2505b5d3902c34ec1915ad4e0
1552980358 <1552980358@qq.com>
2025-11-29 09:44:51 +0800

    configuration: Implement function `Configuration::new`

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,14 +1,33 @@
 pub mod error;
+pub mod cloudflare;
+pub mod config;
+
 mod argument;
-mod cloudflare;
-mod config;
 mod proxied;
 
 use error::Error;
+use argument::Argument;
+use cloudflare::{Cloudflare, GetCloudflare};
+use config::{Config, GetConfig};
 use proxied::GetProxied;
 
 pub struct Configuration {
-    // TODO: Placeholder struct declaration
+    pub cloudflare: Cloudflare,
+    pub config: Config,
+    pub proxied: Option<bool>
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
+
+impl Configuration {
+    fn new() -> Result<Self> {
+        let arguments = Argument::all();
+
+        let cloudflare = arguments.get_cloudflare()?;
+        let config = arguments.get_config()?;
+        let proxied = arguments.get_proxied();
+
+        let configuration = Self { cloudflare, config, proxied };
+        Ok(configuration)
+    }
+}
