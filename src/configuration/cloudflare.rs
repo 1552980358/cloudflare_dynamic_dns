b5696eb69490dd3423de9fbec3a59f51f80fda5c
1552980358 <1552980358@qq.com>
2025-12-07 20:24:11 +0800

    configuration: Add on handing cloudflare config argument

--- src/configuration/cloudflare.rs
+++ src/configuration/cloudflare.rs
@@ -1,16 +1,11 @@
-use std::{
-    fs::read_to_string,
-    path::PathBuf
-};
-use log::info;
 use serde::Deserialize;
 
 pub mod domain_name;
 
 use super::{
-    Result,
     argument::Argument,
-    error::Error
+    error::Error,
+    Result
 };
 use domain_name::DomainName;
 
@@ -31,40 +26,27 @@ pub(super) trait GetCloudflare {
 
 impl GetCloudflare for Vec<Argument> {
     fn get_cloudflare(&self) -> Result<Cloudflare> {
-        self.iter()
+        let path_buf = self.iter()
             .find_map(|argument|
                 if let Argument::CloudflareConfig(path_buf) = argument { Some(path_buf.to_owned()) }
                 else { None }
             )
             .unwrap_or_else(|| {
+                use log::info;
                 info!(
                     target: "configuration.cloudflare",
                     r#"Cloudflare json configuration file not specified: Use default path "{}" instead."#,
                     Cloudflare::DEFAULT_PATH
                 );
+                use std::path::PathBuf;
                 PathBuf::from(Cloudflare::DEFAULT_PATH)
-            })
-            .read_json_file()?
-            .deserialize()
-    }
-}
+            });
 
-trait ReadFile {
-    fn read_json_file(&self) -> Result<String>;
-}
+        use std::fs::read_to_string;
+        let cloudflare_config_json_str = read_to_string(&path_buf)
+            .map_err(|_| Error::CloudflareImportFail(path_buf.to_string_lossy().to_string()))?;
 
-impl ReadFile for PathBuf {
-    fn read_json_file(&self) -> Result<String> {
-        read_to_string(self).map_err(|_| Error::CloudflareImportFail)
-    }
-}
-
-trait DeserializeCloudflare {
-    fn deserialize(&self) -> Result<Cloudflare>;
-}
-
-impl DeserializeCloudflare for String {
-    fn deserialize(&self) -> Result<Cloudflare> {
-        serde_json::from_str::<Cloudflare>(self).map_err(|_| Error::CloudflareImportFail)
+        serde_json::from_str(&cloudflare_config_json_str)
+            .map_err(|_| Error::CloudflareImportFail(path_buf.to_string_lossy().to_string()))
     }
 }
--- src/configuration/error.rs
+++ src/configuration/error.rs
@@ -1,4 +1,5 @@
 pub enum Error {
     CloudflareImportFail,
     ConfigImportFail
+    CloudflareImportFail(String),
 }
